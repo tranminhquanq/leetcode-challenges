aa6960561241d1677f72d55eb95c54189e185287
tranminhquang <quang.tm@teko.vn>
2021-08-17 22:14:16 +0700

    added insertionSort

--- sorting/SortingAlgorithms.java
+++ sorting/SortingAlgorithms.java
@@ -37,10 +37,25 @@ public class SortingAlgorithms {
     }
   }
 
+  public static void insertionSort(int[] array) {
+    int n = array.length;
+    for (int i = 1; i < n; i++) {
+      int j = i - 1;
+      int ai = array[i];
+
+      while (j >= 0 && array[j] > ai) {
+        array[j + 1] = array[j];
+        j--;
+      }
+      array[j + 1] = ai;
+    }
+  }
+
   public static void main(String[] args) {
     int[] input = { 5, 4, 3, 2, 1 };
     // bubbleSort(input);
-    selectionSort(input);
+    // selectionSort(input);
+    insertionSort(input);
     System.out.println(Arrays.toString(input));
   }
 

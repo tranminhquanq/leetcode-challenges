03f8f5e5e2f9b2a0e70084a03c35cc5eb86d066a
tranminhquang <quang.tm@teko.vn>
2021-08-18 23:09:32 +0700

    refactor fibonnacci

--- recursion/_509_Fibonacci_Number.java
+++ recursion/_509_Fibonacci_Number.java
@@ -1,11 +1,18 @@
 public class _509_Fibonacci_Number {
 
   public static int fibonnaci(int n) {
-    if (n == 0)
-      return 0;
-    if (n == 1)
-      return 1;
-    return fibonnaci(n - 1) + fibonnaci(n - 2);
+    int a = 0;
+    int b = 1;
+    int c;
+
+    if (n == 0 || n == 1)
+      return n;
+    for (int i = 2; i <= n; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
   }
 
   public static void main(String[] args) {

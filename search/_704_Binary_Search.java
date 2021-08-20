e213ce2a2ceab1bc86e023da0149c229bebd555a
tranminhquang <quang.tm@teko.vn>
2021-08-20 22:17:04 +0700

    rename function

--- search/_704_Binary_Search.java
+++ search/_704_Binary_Search.java
@@ -1,5 +1,5 @@
 public class _704_Binary_Search {
-  public static int binnarSearch(int[] nums, int target) {
+  public static int binarySearch(int[] nums, int target) {
     int left = 0, right = nums.length - 1;
     while (left <= right) {
       int mid = (left + right) / 2;
@@ -14,7 +14,7 @@ public class _704_Binary_Search {
     return -1;
   }
   
-  public static int binnarSearchUsingRecursion(int[] nums, int target, int left, int right) {
+  public static int binarySearchUsingRecursion(int[] nums, int target, int left, int right) {
     if (left > right)
       return -1;
 
@@ -22,20 +22,20 @@ public class _704_Binary_Search {
     if (nums[mid] == target) {
       return mid;
     } else if (nums[mid] < target) {
-      return binnarSearchUsingRecursion(nums, target, mid + 1, right);
+      return binarySearchUsingRecursion(nums, target, mid + 1, right);
     } else {
-      return binnarSearchUsingRecursion(nums, target, left, mid - 1);
+      return binarySearchUsingRecursion(nums, target, left, mid - 1);
     }
   }
   
-  public static int binnarSearchUsingRecursion(int[] nums, int target) {
+  public static int binarySearchUsingRecursion(int[] nums, int target) {
     int n = nums.length;
-    return binnarSearchUsingRecursion(nums, target, 0, n - 1);
+    return binarySearchUsingRecursion(nums, target, 0, n - 1);
   }
 
   public static void main(String[] args) {
     int[] intput_1 = {-1,0,3,5,9,12};
-    // System.out.println(binnarSearch(intput_1, 9));
-    System.out.println(binnarSearchUsingRecursion(intput_1, 9));
+    // System.out.println(binarySearch(intput_1, 9));
+    System.out.println(binarySearchUsingRecursion(intput_1, 9));
   }
 }

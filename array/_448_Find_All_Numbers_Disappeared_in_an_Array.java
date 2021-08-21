c7b517e54fe020c99e9be68c9eb776b43070e868
tranminhquangg <quangtmps12135@fpt.edu.vn>
2021-08-21 21:24:44 +0700

    fixed

--- array/_448_Find_All_Numbers_Disappeared_in_an_Array.java
+++ array/_448_Find_All_Numbers_Disappeared_in_an_Array.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class _448_Find_All_Numbers_Disappeared_in_an_Array {
-  public List<Integer> findDisappearedNumbers(int[] nums) {
+  public static List<Integer> findDisappearedNumbers(int[] nums) {
     ArrayList<Integer> result = new ArrayList<>();
     boolean[] isVisited = new boolean[nums.length + 1];
     for (int i = 0; i < nums.length; i++) {
@@ -16,6 +16,6 @@ public class _448_Find_All_Numbers_Disappeared_in_an_Array {
     return result;
   }
   public static void main(String[] args) {
-    System.out.println(new _448_Find_All_Numbers_Disappeared_in_an_Array().findDisappearedNumbers(new int[]{4,3,2,7,8,2,3,1}));
+    System.out.println(findDisappearedNumbers(new int[]{4,3,2,7,8,2,3,1}));
   }
 }
